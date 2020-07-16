316cedd8ccc120851b3657fc42e37995e4d6341c
cjcoughlan <cale_coughlan@yahoo.com>
2020-07-16 10:43:08 -0500

    Initial Commit

---
+++ TDBackend/TDBackend/Controllers/ListItemsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TDBackend;
+using TDBackend.Models;
+
+namespace TDBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListItemsController : ControllerBase
+    {
+        private readonly TDContext _context;
+
+        public ListItemsController(TDContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ListItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ListItem>>> GetListItems()
+        {
+            return await _context.ListItems.ToListAsync();
+        }
+
+        // GET: api/ListItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ListItem>> GetListItem(int id)
+        {
+            var listItem = await _context.ListItems.FindAsync(id);
+
+            if (listItem == null)
+            {
+                return NotFound();
+            }
+
+            return listItem;
+        }
+
+        // PUT: api/ListItems/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutListItem(int id, ListItem listItem)
+        {
+            if (id != listItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(listItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ListItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ListItems
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<ListItem>> PostListItem(ListItem listItem)
+        {
+            _context.ListItems.Add(listItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetListItem", new { id = listItem.Id }, listItem);
+        }
+
+        // DELETE: api/ListItems/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ListItem>> DeleteListItem(int id)
+        {
+            var listItem = await _context.ListItems.FindAsync(id);
+            if (listItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.ListItems.Remove(listItem);
+            await _context.SaveChangesAsync();
+
+            return listItem;
+        }
+
+        private bool ListItemExists(int id)
+        {
+            return _context.ListItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TDBackend/TDBackend/TDContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TDBackend.Models;
+
+namespace TDBackend
+{
+    public class TDContext : DbContext
+    {
+        public TDContext(DbContextOptions<TDContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ListItem> ListItems { get; set; }
+    }
+}
